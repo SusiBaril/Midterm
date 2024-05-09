22bc65a5e982e90c0df3527c421681c78536ecf6
SusiBaril <rosaleskeegan@gmail.com>
2024-05-09 21:27:27 +0800

    yes

--- scripts.js
+++ scripts.js
@@ -13,20 +13,20 @@ let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('nav a');
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
 
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(link => {
-                link.classList.remove('active');
-            });
-            document.querySelector('nav a[href="#' + id + '"]').classList.add('active');
-        }
-    });
-};
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(link => {
+        link.classList.remove('active');
+      });
+      document.querySelector('nav a[href="#' + id + '"]').classList.add('active');
+    }
+  });
+}
 
 //alert that the message is sent
 function sendMessage() {
